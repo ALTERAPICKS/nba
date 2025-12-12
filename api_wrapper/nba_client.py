b573677199cfe71c2d04a687d9e0fd3010b8b2a8
ALTERAPICKS <233465192+ALTERAPICKS@users.noreply.github.com>
2025-12-12 14:36:10 -0500

    Add function to get team dashboard metrics

--- api_wrapper/nba_client.py
+++ api_wrapper/nba_client.py
@@ -1,1 +1,16 @@
+from nba_api.stats.endpoints import teamdashboardbygeneralsplits
+
+def get_team_dashboard(team_id: int, last_n_games: int):
+    response = teamdashboardbygeneralsplits.TeamDashboardByGeneralSplits(
+        team_id=team_id,
+        last_n_games=last_n_games
+    )
+
+    df = response.get_data_frames()[0]
+
+    return {
+        "team_id": team_id,
+        "last_n_games": last_n_games,
+        "metrics": df.iloc[0].to_dict()
+    }
 
